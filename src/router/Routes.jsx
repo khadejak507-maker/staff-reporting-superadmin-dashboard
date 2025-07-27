6d442d5a35fed757564f49e06fbd68c9e70a3ec2
Rabeya Akter <rabeyariya251@gmail.com>
2025-07-27 11:41:32 +0600

    implement private route

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -5,24 +5,10 @@ import ForgatePassword from "../Pages/Auth/ForgatePassword/ForgatePassword";
 import Newpass from "../Pages/Auth/NewPass/Newpass";
 import VerifyPass from "../Pages/Auth/VerifyPass/VerifyPass";
 import ContinuePage from "../Pages/Auth/ContinuePage/ContinuePage";
-
-import AboutUs from "../Pages/Settings/AboutUS/AboutUs";
-import PrivacyPolicy from "../Pages/Settings/PrivacyPolicy/PrivacyPolicy";
-import TermsCondition from "../Pages/Settings/TermsCondition/TermsCondition";
 import AdminProfile from "../Pages/AdminProfile/AdminProfile";
-import Notifications from "../Pages/Notification/Notification";
-import StaffDetails from "../Pages/StaffDetails/StaffDetails";
-import AddClients from "../Pages/AddClients/AddClients";
-import Expences from "../Pages/Expences/Expences";
-import Reports from "../Pages/Reports/Reports";
-import CreateStaff from "../Pages/CreateStaff/CreateStaff";
-import OfficeTimeAndNotice from "../Pages/OfficeTimeAndNotice/OfficeTimeAndNotice";
 import SIgnUp from "../Pages/Auth/SignUp/SIgnUp";
-import CalenderComponent from "../Components/Dashboard/Calender/CalenderComponent";
-import Notes from "../Components/Notes/Notes";
-import ClientNotes from "../Components/ClientNotes/ClientNotes";
-import AddLogoAndColor from "../Pages/Settings/AddLogoAndColor/AddLogoAndColor";
-
+import Reports from "../Pages/Reports/Reports";
+import PrivateRoute from "./PrivateRoute";
 export const router = createBrowserRouter([
   {
     path: "/sign-up",
@@ -51,78 +37,22 @@ export const router = createBrowserRouter([
     element: <ContinuePage />,
   },
   {
-    path: "/",
-    element: <MainLayout />,
+    element: <PrivateRoute />,
     children: [
       {
         path: "/",
-        // element: <Analytics />,
-        element: <Reports />,
-      },
-      // {
-      //   path: "/staff-details",
-      //   element: <StaffDetails />,
-      // },
-      // {
-      //   path: "/add-client",
-      //   element: <AddClients />,
-      // },
-      // {
-      //   path: "expenses",
-      //   element: <Expences />,
-      // },
-      // {
-      //   path: "daily-report",
-      //   element: <Reports />,
-      // },
-      // {
-      //   path: "create-staff",
-      //   element: <CreateStaff />,
-      // },
-      // {
-      //   path: "office-time",
-      //   element: <OfficeTimeAndNotice />,
-      // },
-      // {
-      //   path: "/notification",
-      //   element: <Notifications />,
-      // },
+        element: <MainLayout />,
+        children: [
+          {
+            path: "/",
+            element: <Reports />,
+          },
 
-      // setting:
-      // {
-      //   path: "/settings/about-us",
-      //   element: <AboutUs />,
-      // },
-      // {
-      //   path: "/settings/contact-us",
-      //   element: <ContactUs />,
-      // },
-
-      // {
-      //   path: "/settings/privacy-policy",
-      //   element: <PrivacyPolicy />,
-      // },
-      // {
-      //   path: "/settings/terms-condition",
-      //   element: <TermsCondition />,
-      // },
-      // {
-      //   path: "/settings/add-logo-and-color",
-      //   element: <AddLogoAndColor />,
-      // },
-      // {
-      //   path: "/staff-details/notes/:id",
-      //   element: <Notes />,
-      // },
-      // {
-      //   path: "/add-client/notes/:id",
-      //   element: <ClientNotes />,
-      // },
-
-      // Admin profile:
-      {
-        path: "/admin-profile",
-        element: <AdminProfile />,
+          {
+            path: "/admin-profile",
+            element: <AdminProfile />,
+          },
+        ],
       },
     ],
   },
